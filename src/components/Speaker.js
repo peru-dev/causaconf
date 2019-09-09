3a8bc8212d661d68856979a6ba6b3b3be3255496
Lupo Montero <lupomontero@gmail.com>
2019-09-09 10:37:13 -0500

    feat(Speaker): Uses twitter avatar if speaker.twitter present, otherwise defaults to speaker.avatar

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -1,12 +1,12 @@
 import { createElement } from 'nano';
 
 
-const SpeakerAvatar = ({ speaker }) => (
-  (speaker.twitter)
+const SpeakerAvatar = ({ src, alt }) => (
+  src
     ? createElement('img', {
       className: 'avatar',
-      src: `https://twitter.com/${speaker.twitter}/profile_image?size=original`,
-      alt: speaker.name,
+      src,
+      alt,
     })
     : createElement('div', {
       className: 'avatar',
@@ -18,7 +18,14 @@ const SpeakerAvatar = ({ speaker }) => (
 export default ({ speaker }) => createElement('a', {
   href: speaker.twitter ? `https://twitter.com/${speaker.twitter}` : '#',
   children: [
-    SpeakerAvatar({ speaker }),
+    SpeakerAvatar({
+      alt: speaker.name,
+      src: (
+        speaker.twitter
+          ? `https://twitter.com/${speaker.twitter}/profile_image?size=original`
+          : speaker.avatar
+      ),
+    }),
     createElement('h3', { innerText: speaker.name }),
     speaker.twitter && createElement('p', { innerText: `@${speaker.twitter}` }),
   ],
