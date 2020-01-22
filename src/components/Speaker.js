2c778e3f49b68795eaedbefd37889885888aa343
MistyBlunch <gracenikole@gmail.com>
2020-01-22 02:03:31 -0500

    speakers images added and json modified to show the speakers

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -1,32 +1,27 @@
-import createElement from '../lib/createElement';
+import createElement from "../lib/createElement";
 
-
-const SpeakerAvatar = ({ src, alt }) => (
+const SpeakerAvatar = ({ src, alt }) =>
   src
-    ? createElement('img', {
-      className: 'avatar',
-      src,
-      alt,
-    })
-    : createElement('div', {
-      className: 'avatar',
-      innerText: '?',
-    })
-);
+    ? createElement("img", {
+        className: "avatar",
+        src,
+        alt
+      })
+    : createElement("div", {
+        className: "avatar",
+        innerText: "?"
+      });
 
-
-export default ({ speaker }) => createElement('a', {
-  href: speaker.twitter ? `https://twitter.com/${speaker.twitter}` : '#',
-  children: [
-    SpeakerAvatar({
-      alt: speaker.name,
-      src: (
-        speaker.twitter
-          ? `https://twitter.com/${speaker.twitter}/profile_image?size=original`
-          : speaker.avatar
-      ),
-    }),
-    createElement('h3', { innerText: speaker.name }),
-    speaker.twitter && createElement('p', { innerText: `@${speaker.twitter}` }),
-  ],
-});
+export default ({ speaker }) =>
+  createElement("a", {
+    href: speaker.twitter ? `https://twitter.com/${speaker.twitter}` : "#",
+    children: [
+      SpeakerAvatar({
+        alt: speaker.name,
+        src: speaker.avatar
+      }),
+      createElement("h3", { innerText: speaker.name }),
+      speaker.twitter &&
+        createElement("p", { innerText: `@${speaker.twitter}` })
+    ]
+  });
