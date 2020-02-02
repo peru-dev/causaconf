05ecf626ad1e6735408c997a1a1131753986b3a3
MistyBlunch <gracenikole@gmail.com>
2020-02-02 13:59:50 -0500

    Comillas modificadas

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -1,27 +1,27 @@
-import createElement from "../lib/createElement";
+import createElement from '../lib/createElement';
 
-const SpeakerAvatar = ({ src, alt }) =>
+const SpeakerAvatar = ({ src, alt }) => (
   src
-    ? createElement("img", {
-        className: "avatar",
+    ? createElement('img', {
+        className: 'avatar',
         src,
         alt
       })
-    : createElement("div", {
-        className: "avatar",
-        innerText: "?"
-      });
-
+    : createElement('div', {
+        className: 'avatar',
+        innerText: '?'
+      })
+  );
 export default ({ speaker }) =>
-  createElement("a", {
-    href: speaker.twitter ? `https://twitter.com/${speaker.twitter}` : "#",
+  createElement('a', {
+    href: speaker.twitter ? `https://twitter.com/${speaker.twitter}` : '#',
     children: [
       SpeakerAvatar({
         alt: speaker.name,
         src: speaker.avatar
       }),
-      createElement("h3", { innerText: speaker.name }),
+      createElement('h3', { innerText: speaker.name }),
       speaker.twitter &&
-        createElement("p", { innerText: `@${speaker.twitter}` })
+        createElement('p', { innerText: `@${speaker.twitter}` })
     ]
   });
